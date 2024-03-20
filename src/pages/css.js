8c6e762c4ec245c45c84766047b3e676df80c85e
ahdbot <ahdbot3@gmail.com>
2024-03-20 23:28:24 +0300

    commit

---
+++ src/pages/css.js
@@ -0,0 +1,23 @@
+import Header from "../comp/header";
+import Footer from "../comp/Footer";
+
+import { Helmet } from "react-helmet-async";
+
+const Css = () => {
+  return (
+    <>
+      <Helmet>
+        <title>CSS Page</title>
+        <meta name="description" content="csssssssssssssssssssss" />
+      </Helmet>
+
+      <Header />
+
+    
+
+      <Footer />
+    </>
+  );
+};
+
+export default Css;
